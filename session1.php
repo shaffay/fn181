799a18bed0e502fc9803c95cff17d1b5019e5481
Shaffay Bajwa <33603579+shaffay@users.noreply.github.com>
2022-05-17 15:55:05 +0500

    Initial commit

---
+++ session1.php
@@ -0,0 +1,23 @@
+<?php
+
+// $name = "Ali Ahmed";
+
+
+
+
+
+
+// echo "Hello ".$name;
+
+
+$a = 10 ;
+$b = 20 ;
+
+$z = 22;
+$c = $a + $b + $z;
+
+echo $c;
+
+
+
+?>
---
+++ session2.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark">
+
+
+  <div class="container text-light">
+  
+  <form action="session2.php" method="post" >
+
+  <div class="form-group">
+    <label for="">First Number</label>
+    <input type="number" class="form-control" name="num1" >
+  </div>
+
+  <div class="form-group">
+    <label for="">Second Number</label>
+    <input type="number" class="form-control" name="num2" >
+  </div>
+
+
+  <input type="submit" value="Add" name="btn" class="btn btn-info float-right" >
+
+  </form>
+
+  </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<?php
+
+if(isset($_POST['btn'])){
+
+    $n1 = $_POST['num1'];
+    $n2 = $_POST['num2'];
+    
+    $add = $n1 + $n2;
+    ?>
+    <script>alert("The Addition of Num1 & Num2 ="+<?php echo $add ?>)</script>
+
+<?php
+}
+
+
+
+
+?>
